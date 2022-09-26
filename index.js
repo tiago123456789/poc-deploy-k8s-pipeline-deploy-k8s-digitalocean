b7d60465f4e005876ea683e1afb1c6c1bbf83e07
Tiago rosa da costa <tiagorosadacost@gmail.com>
2022-09-25 22:20:11 -0300

    Update code to force update application running into the k8s

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.write('Hello World! V2');
+  res.write('Hello World! Application updated V3');
   res.end();
 }).listen(3000);
